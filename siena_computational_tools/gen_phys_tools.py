bc1d3bec39121b366bb20666284a7b8e1e81831e
mattbellis <matthew.bellis@gmail.com>
2020-09-17 17:28:49 -0400

    Stuff

--- siena_computational_tools/gen_phys_tools.py
+++ siena_computational_tools/gen_phys_tools.py
@@ -9,6 +9,9 @@ from scipy.optimize import curve_fit
 # over your data points
 ################################################################################
 
+def quadratic_func(x,a,b,c): 
+     return a*x**2 + b*x + c
+
 def linear_func(x,b,m): 
      return m*x+b
 
@@ -39,4 +42,36 @@ def linear_fit_to_data(xpts, ypts, yerr, starting_parameters=[1.0, 1.0], verbose
 
 
   return slope, slope_uncert, intercept, intercept_uncert, fit_xpts, fit_ypts, chi2, ndof
+
+################################################################################
+def quadratic_fit_to_data(xpts, ypts, yerr, starting_parameters=[1.0, 1.0, 1.0], verbose=False):
+
+  print('Fit with f(x)=ax^2 + bx + c')
+
+  popt, pcov = curve_fit(quadratic_func, xpts, ypts, p0=starting_parameters, sigma=yerr, absolute_sigma=True)
+
+  #print(popt, pcov)
+
+  a = popt[0]
+  a_uncert = np.sqrt(pcov[0][0])
+
+  b = popt[1]
+  b_uncert = np.sqrt(pcov[1][1])
+
+  c = popt[2]
+  c_uncert = np.sqrt(pcov[2][2])
+
+  fit_xpts = np.linspace(min(xpts),max(xpts),100)
+  fit_ypts = linear_func(fit_xpts, intercept, slope)
+
+  # Calculate a chi square value 
+  chi2 = ((((slope*xpts + intercept) - ypts)**2)/(yerr**2)).sum()
+  ndof = len(xpts) - 2
+
+  if verbose:
+      print('a = {0:.3} +/- {1:.3}\nb = {2:.3} +/- {3:.3}\nc = {4:.3} +/- {5:.3}'.format(a,a_uncert, b, c_uncert, b, c_uncert))
+      print('Chi2 = {0:.3f}  #dof = {1}'.format(chi2, ndof))
+
+
+  return a, a_uncert, b, c_uncert, b, c_uncert, fit_xpts, fit_ypts, chi2, ndof
 
